95c185d7601822ef66886c161a36ca93a42a19c1
ankit1016 <56350865+ankit1016@users.noreply.github.com>
2020-04-08 10:49:18 +0530

    Add files via upload

---
+++ 19bca1190 p7 part1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"enter two number ";
+	cin>>a>>b;
+	
+	try
+	{
+		if(b==0)
+		
+		throw b;
+		cout<<"result="<<a/b;
+	}
+	
+	catch(int n)
+	{
+		cout<<"input number is not valid  "<<n;
+	}
+}
+
